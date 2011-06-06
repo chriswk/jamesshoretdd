4b5eaf59a15c0dd8f5338b4fa46dd45f04de23ac
Christopher Kolstad <christopher.kolstad@gmail.com>
2011-06-06 20:11:00 +0200

    Clean up after PODCAST 3

--- src/main/java/com/chriswk/financing/SavingsAccountYear.java
+++ src/main/java/com/chriswk/financing/SavingsAccountYear.java
@@ -12,23 +12,12 @@ public class SavingsAccountYear {
         this.interestRate = interestRate;
     }
 
-    public void withdraw(int amount) {
-        startingBalance -= amount;
-    }
-
-    public void deposit(int amount) {
-        startingBalance += amount;
-    }
-    public int getStartingBalance() {
-        return startingBalance;
-    }
-
     public SavingsAccountYear nextYear() {
         return new SavingsAccountYear(this.endingBalance(), this.interestRate());
     }
 
     public int endingBalance() {
-        return getStartingBalance() + (getStartingBalance() * this.interestRate / 100);
+        return startingBalance() + (startingBalance() * this.interestRate / 100);
     }
 
     public int startingBalance() {
