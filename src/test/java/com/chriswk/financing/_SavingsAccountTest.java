74b93e13446481dfe4db1669149bbe1dd1740a34
Christopher Kolstad <christopher.kolstad@gmail.com>
2011-06-06 19:24:55 +0200

    Initial commit

---
+++ src/test/java/com/chriswk/financing/_SavingsAccountTest.java
@@ -0,0 +1,19 @@
+package com.chriswk.financing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by IntelliJ IDEA.
+ * User: chriswk
+ * Date: 06.06.11
+ * Time: 19:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class _SavingsAccountTest {
+
+    @Test
+    public void nothing() {
+        fail("hi");
+    }
+
+}
